627d73755954d250ffe16f140211caf690eef096
abhitopia <abhitopia@isme.com>
2024-07-22 23:27:50 +0100

    First commit. Added all the code.

---
+++ src/task.py
@@ -0,0 +1,249 @@
+
+#%%
+from enum import Enum, auto
+import json
+from pathlib import Path
+from typing import List, Optional
+import numpy as np
+
+
+class ArcTask:
+    def __init__(self, id, train, test, dataset=None, version=None, augmentation_group="None"):
+        self.dataset = dataset
+        self.version = version
+        self.augmentation_group = augmentation_group
+        self.id = id
+        self.train = [(np.array(example['input']), np.array(example['output'])) for example in train]
+        self.test = [(np.array(example['input']), np.array(example['output'])) for example in test]
+
+    def __repr__(self):
+        return f'ArcTask(id={self.id}, dataset={self.dataset}, version={self.version}, augmentation_group={self.augmentation_group})'
+
+    def __eq__(self, other):
+        if not isinstance(other, ArcTask):
+            return False
+
+        def check_examples(examples1, examples2):
+            if len(examples1) != len(examples2):
+                return False
+
+            for i in range(len(examples1)):
+                if not np.array_equal(examples1[i][0], examples2[i][0]):
+                    return False
+                if not np.array_equal(examples1[i][1], examples2[i][1]):
+                    return False
+            return True
+        
+        return check_examples(self.train, other.train) and check_examples(self.test, other.test)
+    
+    def __lt__(self, other):
+        if not isinstance(other, ArcTask):
+            return NotImplemented
+        return (self.id, self.version) < (other.id, other.version)
+
+
+
+class ColorPermutation(Enum):
+    CPID = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # Identity
+    CP01 = [7, 4, 5, 2, 8, 3, 0, 9, 6, 1]
+    CP02 = [0, 9, 4, 5, 6, 8, 1, 3, 2, 7]
+    CP03 = [7, 4, 1, 9, 6, 0, 8, 2, 5, 3]
+    CP04 = [9, 6, 5, 7, 4, 0, 3, 8, 1, 2]
+    CP05 = [1, 8, 0, 3, 9, 5, 6, 2, 7, 4]
+    CP06 = [5, 3, 1, 9, 7, 6, 0, 2, 8, 4]
+    CP07 = [1, 4, 3, 8, 7, 9, 6, 2, 5, 0]
+    CP08 = [6, 0, 2, 1, 3, 4, 7, 8, 5, 9]
+    CP09 = [2, 0, 3, 8, 4, 6, 1, 9, 5, 7]
+
+    @property
+    def transform(self):
+        colors = self.value
+        color_mapping = {original: new for original, new in enumerate(colors)}
+        return lambda x: np.vectorize(color_mapping.get)(x)
+
+
+class ArrayTransform(Enum):
+    IDENT = auto()
+    RT090 = auto()
+    RT180 = auto()
+    RT270 = auto()
+    FLPLR = auto()
+    FLPUD = auto()
+    FLPDG = auto()
+    FLPAD = auto()
+
+    @property
+    def transform(self):
+        return {
+            'IDENT': lambda x: x,
+            'RT090': lambda x: np.rot90(x),
+            'RT180': lambda x: np.rot90(x, k=2),
+            'RT270' : lambda x: np.rot90(x, k=3),
+            'FLPLR': lambda x: np.fliplr(x),
+            'FLPUD': lambda x: np.flipud(x),
+            'FLPDG': lambda x: np.flipud(np.rot90(x)),
+            'FLPAD': lambda x: np.fliplr(np.rot90(x)),           
+        }[self.name]
+
+
+class TaskInvariantTransform:
+    def __init__(self, array_transform: ArrayTransform, color_perm: ColorPermutation):
+        self.arr_transform = array_transform
+        self.color_perm = color_perm
+    
+    @property
+    def name(self):
+        return f'{self.arr_transform.name}_{self.color_perm.name}'
+
+    def __call__(self, task: ArcTask) -> ArcTask:
+
+        task_copy = ArcTask(id=task.id,
+                            train=[],
+                            test=[],
+                            dataset=task.dataset,
+                            version=self.name)
+        
+        task_copy.train = task.train
+        task_copy.test = task.test
+
+        for transform in [self.arr_transform.transform, self.color_perm.transform]:
+            task_copy.train = [(transform(example[0]), transform(example[1])) for example in task_copy.train]
+            task_copy.test = [(transform(example[0]), transform(example[1])) for example in task_copy.test]
+
+        return task_copy
+    
+
+class TaskAugmenter:
+    def __init__(self, augmentation_id: int):
+        self.transformation_groups = TaskAugmenter.transformation_groups()
+        assert 0 <= augmentation_id < len(self.transformation_groups), f'Invalid augmentation_id: {augmentation_id}'
+        self.group_id = augmentation_id
+        
+    @staticmethod
+    def transformation_groups():
+        List_A = list(ArrayTransform)
+        List_B = list(ColorPermutation)
+        groups = []
+        for i in range(len(List_B)):
+            group = [(List_A[j % len(List_A)], List_B[(j + i) % len(List_B)]) for j in range(len(List_A))]
+            groups.append(group)
+
+        return groups
+
+    def __call__(self, task: ArcTask, remove_redundant=True) -> ArcTask:
+        group = self.transformation_groups[self.group_id]
+        augmented_tasks = []
+        array_transformed_tasks = []
+        for array_transform, color_transform in group:
+            # Apply only the array transformation
+            no_color_transform = TaskInvariantTransform(array_transform, ColorPermutation.CPID)
+            array_transformed_tasks.append(no_color_transform(task))
+            transform = TaskInvariantTransform(array_transform, color_transform)
+            transformed_task = transform(task)
+
+            # Change the dataset name to include the transformation group id
+            transformed_task.augmentation_group = f'{self.group_id:02d}'
+            augmented_tasks.append(transformed_task)
+
+        if remove_redundant:
+            # Remove all redundant tasks where array transformation is the same
+            redundant_tasks = set()
+            for i in range(len(array_transformed_tasks)):
+                if i in redundant_tasks:
+                    continue
+                for j in range(i+1, len(array_transformed_tasks)):
+                    if array_transformed_tasks[i] == array_transformed_tasks[j]:
+                        redundant_tasks.add(j)
+
+            augmented_tasks = [task for i, task in enumerate(augmented_tasks) if i not in redundant_tasks]
+
+        return augmented_tasks
+    
+
+class ArcTasksLoader:
+    def __init__(self, name: str, path: str):
+        base_path = Path(__file__).resolve().parent.parent    
+        self.path = base_path / Path(path)
+        self.name = name
+
+    def __lt__(self, other):
+        if not isinstance(other, ArcTasksLoader):
+            return NotImplemented
+        return self.name < other.name
+
+    def json_files(self):
+        return [json for json in Path(self.path).glob("**/*.json")]
+    
+    def _load_tasks(self) -> List[ArcTask]:
+        tasks = []
+        for f in self.json_files():
+            task_json = json.load(f.open('r'))
+            task_id = self.name + "_" + f.stem
+            task = ArcTask(id=task_id,
+                           train=task_json['train'],
+                           test=task_json['test'],
+                           dataset=self.name,
+                           version='original')
+            tasks.append(task)
+        return tasks
+    
+    def load_tasks(self, augmentation_id: Optional[int] = None) -> List[ArcTask]:
+        tasks = self._load_tasks()
+
+        if augmentation_id is None:
+            return tasks
+        
+        assert isinstance(augmentation_id, int), 'augmentation_id must be an integer'
+        assert 0 <= augmentation_id < len(TaskAugmenter.transformation_groups()), f'Invalid augmentation_id: {augmentation_id}'
+        
+        augmented_tasks = []
+        task_augmenter = TaskAugmenter(augmentation_id)
+        for task in tasks:
+            augmented_tasks.extend(task_augmenter(task))
+
+        # Returns after sorting so results are reproducible
+        return sorted(augmented_tasks)
+
+
+# Training Tasks
+ARC_1D = ArcTasksLoader(name='1D-ARC', path='datasets/arc-dataset-collection/dataset/1D-ARC/data')
+ARC_TRAIN = ArcTasksLoader(name='ARC_TRAIN', path='datasets/arc-dataset-collection/dataset/ARC/data/training')
+ARC_SYTH_EXTEND = ArcTasksLoader(name='ARC_SYTH_EXTEND', path='datasets/arc-dataset-collection/dataset/ARC_synthetic_extend/data')
+ARC_COMMUNITY = ArcTasksLoader(name='ARC_COMMUNITY', path='datasets/arc-dataset-collection/dataset/arc-community/data')
+ARC_DIVA = ArcTasksLoader(name='ARC_DIVA', path='datasets/arc-dataset-collection/dataset/arc-dataset-diva/data')
+ARC_CONCEPT = ArcTasksLoader(name='ARC_CONCEPT', path='datasets/arc-dataset-collection/dataset/ConceptARC/data')
+ARC_DBIGHAM = ArcTasksLoader(name='ARC_DBIGHAM', path='datasets/arc-dataset-collection/dataset/dbigham/data')
+ARC_MINI = ArcTasksLoader(name='ARC_MINI', path='datasets/arc-dataset-collection/dataset/Mini-ARC/data')
+ARC_NOSOUND = ArcTasksLoader(name='ARC_NOSOUND', path='datasets/arc-dataset-collection/dataset/nosound/data')
+ARC_PQA = ArcTasksLoader(name='ARC_PQA', path='datasets/arc-dataset-collection/dataset/PQA/data')
+ARC_REARC_EASY = ArcTasksLoader(name='ARC_REARC_EASY', path='datasets/arc-dataset-collection/dataset/RE-ARC/data/easy')
+ARC_REARC_HARD = ArcTasksLoader(name='ARC_REARC_HARD', path='datasets/arc-dataset-collection/dataset/RE-ARC/data/hard')
+ARC_SEQUENCE = ArcTasksLoader(name='ARC_SEQUENCE', path='datasets/arc-dataset-collection/dataset/Sequence_ARC/data')
+ARC_SYNTH_RIDDLES = ArcTasksLoader(name='ARC_SYNTH_RIDDLES', path='datasets/arc-dataset-collection/dataset/synth_riddles/data')
+ARC_EVAL = ArcTasksLoader(name='ARC_EVAL', path='datasets/arc-dataset-collection/dataset/ARC/data/evaluation')
+
+
+TRAINING_TASKLOADER = ARC_TRAIN
+EVALUATION_TASKLOADER = ARC_EVAL
+
+AUXILIARY_TASKLOADERS = [
+    ARC_1D,
+    ARC_SYTH_EXTEND,
+    ARC_COMMUNITY,
+    ARC_DIVA,
+    ARC_CONCEPT,
+    ARC_DBIGHAM,
+    ARC_MINI,
+    ARC_NOSOUND,
+    ARC_PQA,
+    ARC_REARC_EASY,
+    ARC_REARC_HARD,
+    ARC_SEQUENCE,
+    ARC_SYNTH_RIDDLES
+]
+
+
+# Evaluation Tasks
+#%%
+
+# %%
---
+++ train.py
@@ -0,0 +1,256 @@
+
+#%%
+import time
+import math
+import torch
+import logging
+from src.dataset import TrainingData, ProgramTokenizer, GridTokenizer
+from src.interpreter import Interpreter, InterpreterConfig
+from src.utils import nearest_greater_power_of_2
+#%%
+
+# Training Data Configuration
+BS = 2
+SEQ_LEN = 1024
+DYNAMMIC_BATCHING = True
+AUGMENTATION_FACTOR = 2
+JOIN_VERSION = False
+DATA_DEVICE = 'cpu'
+PIN_MEMORY = False
+
+# Global Configuration
+SEED = 42
+
+
+training_data = TrainingData(augmentation_factor=AUGMENTATION_FACTOR,
+                            join_version=JOIN_VERSION, 
+                            seed=SEED).load()
+
+trains_ds = training_data.train_ds.subset(0, 2000)
+eval_ds = training_data.eval_ds.subset(0, 2000)
+train_dl = trains_ds.get_dataloader(batch_size=BS,
+                                    seq_len=SEQ_LEN,
+                                    batch_by_token_count=DYNAMMIC_BATCHING,
+                                    device=torch.device(DATA_DEVICE),
+                                    pin_memory=PIN_MEMORY)
+eval_dl = eval_ds.get_dataloader(batch_size=BS,
+                                seq_len=SEQ_LEN,
+                                batch_by_token_count=DYNAMMIC_BATCHING,
+                                device=torch.device(DATA_DEVICE),
+                                pin_memory=PIN_MEMORY)
+
+
+program_tokenizer = training_data.program_tokenizer
+grid_tokenizer = training_data.grid_tokenizer
+
+PROGRAM_VOCAB_SIZE = nearest_greater_power_of_2(len(program_tokenizer))
+GRID_VOCAB_SIZE = nearest_greater_power_of_2(len(grid_tokenizer))
+
+print(f"Program Vocab Size: {PROGRAM_VOCAB_SIZE}")
+print(f"Grid Vocab Size: {GRID_VOCAB_SIZE}")
+#%%
+
+## Model Set up
+
+N_LAYERS = 1
+N_MIXERS = 1
+N_BLOCKS = 1
+N_HEADS = 8
+N_DIM = 64
+
+model_config = InterpreterConfig(
+    prog_vocab_size = PROGRAM_VOCAB_SIZE,
+    grid_vocab_size = GRID_VOCAB_SIZE,
+    n_dim = N_DIM, # dimension of the model
+    n_head = N_HEADS, # number of heads within each self-attention block
+    n_mixers = N_MIXERS, # number of self-attention layers within each transformer block
+    n_blocks = N_BLOCKS, # number of transformer blocks within each recurrence block
+    n_rec_layers = N_LAYERS # number of recurrences
+)
+
+model = Interpreter(model_config,
+                    prog_tokenizer=program_tokenizer,
+                    grid_tokenizer=grid_tokenizer)
+
+
+# Training Set up
+
+MODEL_WD = 0.01
+MODEL_LR = 1
+PROG_LR_SCALE = 10
+PROG_WD_SCALE = 0.0
+PROGRAM_SCALE = 0.1   # <1 So program embeddings are moved slower across batches.
+TRAIN_DEVICE = 'cpu'
+
+optimizer = model.get_optimizer(model_weight_decay=MODEL_WD,
+                                model_lr=MODEL_LR,
+                                prog_lr_scale=PROGRAM_SCALE,
+                                device_type=TRAIN_DEVICE)
+#%%
+
+from src.trainer import TrainerBase
+
+class ArcTrainer(TrainerBase):
+
+    @staticmethod
+    def _output_target_metrics(logits: torch.Tensor, y: torch.Tensor):
+        _, predicted_tokens = torch.max(logits, dim=2)
+        correct_token_predictions = (predicted_tokens == y)
+        total_correct_tokens = correct_token_predictions.sum()
+        total_tokens = y.numel()
+        # token_accuracy = total_correct_tokens / total_tokens
+
+        # Sample-level accuracy
+        # Check if all tokens in a sequence are correct for each sample
+        correct_samples = correct_token_predictions.all(dim=1)
+        total_correct_samples = correct_samples.sum()
+        total_samples = y.shape[0]
+        # sample_accuracy = total_correct_samples / total_samples
+
+        return {
+            'total_correct_tokens': total_correct_tokens.item(),
+            'total_tokens': total_tokens,
+            'total_correct_samples': total_correct_samples.item(),
+            'total_samples': total_samples
+        }
+    
+    def add_step_metrics(self, metrics_obj, loss, logits, y):
+        batch_metrics = self._output_target_metrics(logits, y)
+        metrics_obj.add_metric('Loss', loss.item())
+        metrics_obj.add_metric('TokenAcc(%)',
+                            batch_metrics['total_correct_tokens']*100, 
+                            batch_metrics['total_tokens'])
+        
+        metrics_obj.add_metric('SampleAcc(%)',
+                            batch_metrics['total_correct_samples']*100,
+                            batch_metrics['total_samples'])
+
+    def add_post_step_metrics(self, metrics_obj, batch_time, num_tokens):
+        # Batch Metrics
+        metrics_obj.add_metric('#Tokens(/s)', num_tokens, (batch_time / 1000))
+        metrics_obj.add_metric('ΔT(ms)', batch_time)
+
+    def pre_train_step(self, batch):
+        self.__train_batch_time_start = time.time()
+
+    def pre_eval_step(self, batch):
+        self.__eval_batch_time_start = time.time()
+    
+    def train_step(self, batch):
+        (p, i), t = batch
+        logits = self.model(p, i)
+        loss = self.model.loss_fn(logits, t)
+        self.add_step_metrics(self.train_metrics, loss, logits, t)
+        return loss
+    
+    def eval_step(self, batch):
+        (p, i), t = batch
+        logits = self.model(p, i)
+        loss = self.model.loss_fn(logits, t)    
+        self.add_step_metrics(self.eval_metrics, loss, logits, t)
+        return loss
+    
+    def post_train_step(self, batch):
+        (_, _), t = batch
+        num_tokens = t.numel()
+        train_batch_time = (time.time() - self.__train_batch_time_start)*1000
+        self.add_post_step_metrics(self.train_metrics, train_batch_time, num_tokens)
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize() # wait for the GPU to finish work
+        elif self.device.type == 'mps':
+            torch.mps.synchronize() # wait for the MPS to finish work
+            torch.mps.empty_cache() # clear the MPS cache
+
+    def post_eval_step(self, batch):
+        (_, _), t = batch
+        num_tokens = t.numel()
+        eval_batch_time = (time.time() - self.__eval_batch_time_start)*1000
+        self.add_post_step_metrics(self.eval_metrics, eval_batch_time, num_tokens)
+
+    def pre_checkpoint_save(self, state_dict):
+        tokenizers = {
+            'program_tokenizer': self.model.prog_tokenizer.to_dict(),
+            'grid_tokenizer': self.model.grid_tokenizer.to_dict()
+        }
+        state_dict['tokenizers'] = tokenizers
+        state_dict['model_config'] = self.model.config.to_dict()
+
+    def post_load_checkpoint(self, state_dict):
+        prog_tokenizer = ProgramTokenizer.from_dict(state_dict['tokenizers']['program_tokenizer'])
+        grid_tokenizer = GridTokenizer.from_dict(state_dict['tokenizers']['grid_tokenizer'])
+        model_config = InterpreterConfig.from_dict(state_dict['model_config'])
+        assert model_config == self.model.config, "Cannot resume, Model Configs do not match!"
+        assert prog_tokenizer == self.model.prog_tokenizer, "Cannot resume, Program Tokenizers do not match!"
+        assert grid_tokenizer == self.model.grid_tokenizer, "Cannot resume, Grid Tokenizers do not match!"
+
+    def multiplicative_lr_factor_schedule(self, step):
+        max_lr = 1.0
+        min_lr = max_lr * 0.05
+        num_step_in_epoch = len(self.train_dl)
+        warmup_steps = num_step_in_epoch * self.hparams['lr_warmup_epochs']
+        max_steps = num_step_in_epoch * self.hparams['lr_decay_epochs']
+
+        # 1) linear warmup for warmup_iters steps
+        if step < warmup_steps:
+            return max_lr * (step + 1) / warmup_steps
+        # 2) if it > lr_decay_iters, return min learning rate
+        if step > max_steps:
+            return min_lr
+        # 3) in between, use cosine decay down to min learning rate
+        decay_ratio = (step - warmup_steps) / (max_steps - warmup_steps)
+        assert 0 <= decay_ratio <= 1
+        coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+        return min_lr + coeff * (max_lr - min_lr)
+        
+
+    @staticmethod
+    def load_model_from_checkpoint(checkpoint_path, device='cpu'):
+        state_dict = torch.load(checkpoint_path, map_location='cpu')
+        model_config = InterpreterConfig.from_dict(state_dict['model_config'])
+        prog_tokenizer = ProgramTokenizer.from_dict(state_dict['tokenizers']['program_tokenizer'])
+        grid_tokenizer = GridTokenizer.from_dict(state_dict['tokenizers']['grid_tokenizer'])
+        model = Interpreter(model_config, prog_tokenizer, grid_tokenizer)
+        model.load_state_dict(state_dict['model_state_dict'])
+        return model.to(device)
+    
+config = {
+    'batch_size': BS,
+    'seq_len': SEQ_LEN,
+    'augmentation_factor': AUGMENTATION_FACTOR,
+    'join_version': JOIN_VERSION,
+    'seed': SEED,
+    'data_device': DATA_DEVICE,
+    'pin_memory': PIN_MEMORY,
+    'n_layers': N_LAYERS,
+    'n_mixers': N_MIXERS,
+    'n_blocks': N_BLOCKS,
+    'n_heads': N_HEADS,
+    'n_dim': N_DIM,
+    'program_vocab_size': PROGRAM_VOCAB_SIZE,
+    'grid_vocab_size': GRID_VOCAB_SIZE,
+    'weight_decay': MODEL_WD,
+    'learning_rate': MODEL_LR,
+    'program_scale': PROGRAM_SCALE,
+    'train_device': TRAIN_DEVICE,
+    'lr_warmup_epochs': 1,
+    'lr_decay_epochs': 4,
+}
+
+    
+trainer = ArcTrainer(
+        experiment_name='arc_setup',
+        run_name='one',
+        eval_interval=50,
+        num_epochs=4,
+        model=model,
+        hparams=config,
+        optimizer=optimizer,
+        train_dl=train_dl,
+        eval_dl=eval_dl,
+        log_level=logging.INFO
+    )
+
+trainer.train()
+
+# %%
