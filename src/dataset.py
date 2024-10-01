09b511c784c72cf3c2633b63d87097e13d4259b4
abhitopia <abhitopia@isme.com>
2024-10-01 20:56:48 +0100

    simplified and fixed dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -19,7 +19,6 @@ class TargetTokenCountBatchSampler(BatchSampler):
 
         self.shuffle = shuffle
         self.min_util = min_util
-        self._dataset_lens = [self.sample_len(x, y) for x, y in self.dataset]
         self.batches = self.create_batches()
         self.shuffle_batches()
 
@@ -33,171 +32,54 @@ class TargetTokenCountBatchSampler(BatchSampler):
         random.shuffle(self.batches)
 
     @staticmethod
-    def sample_len(model_input: MODEL_INPUT, model_output: MODEL_OUTPUT):
-        inp_len = len(model_input.grid)
-        # print(inp_len, model_input.input.size)
-        out_len = len(model_output.grid)
+    def example_len(example: Example):
+        inp_len = example.input.size
+        out_len = example.output.size
         if inp_len == out_len:
             return (0, inp_len, out_len)
         elif inp_len < out_len:
             return (-1, out_len, inp_len)
         else:
             return (1, inp_len, out_len)
-        
-    def merge_batches(self, batches, token_counts, batch_widths):
-        sorted_batch_indices = sorted(range(len(batches)), key=lambda i: (token_counts[i], batch_widths[i]), reverse=False)
 
-        super_batches = []
-
-        super_batch = []
-
-        super_batch_width = 0
-        super_batch_widths = []
-
-        super_batch_token_count = 0
-        super_batch_token_counts = []
-
-        for i in sorted_batch_indices:
-            if token_counts[i] >= self.approx_token_count:
-                super_batches.append(batches[i])
-                super_batch_widths.append(batch_widths[i])
-                super_batch_token_counts.append(token_counts[i])
-                continue
-
-            inc_batch_width = max(batch_widths[i], super_batch_width)
-            inc_batch_token_count = inc_batch_width * (len(batches[i]) + len(super_batch))
-
-            if inc_batch_token_count <= self.approx_token_count:
-                super_batch += batches[i]
-                super_batch_width = inc_batch_width
-                super_batch_token_count = inc_batch_token_count
-            elif super_batch_token_count <= 0.5*self.approx_token_count:
-                # skip current batch and move to next
-                super_batches.append(batches[i])
-                super_batch_widths.append(batch_widths[i])
-                super_batch_token_counts.append(token_counts[i])
-            else:
-                super_batches.append(super_batch)
-                super_batch_widths.append(super_batch_width)
-                super_batch_token_counts.append(super_batch_token_count)
-                super_batch = batches[i]
-                super_batch_width = batch_widths[i]
-                super_batch_token_count = token_counts[i]
-
-
-        if len(super_batch) > 0:
-            super_batches.append(super_batch)
-            super_batch_widths.append(super_batch_width)
-            super_batch_token_counts.append(super_batch_token_count)
-
-        return super_batches, super_batch_token_counts, super_batch_widths
-
-
-    def reduce_num_batches(self, batches, batch_token_counts, batch_widths):
-        # print("Reducing number of batches")
-        mean_util = np.mean([self.batch_utilisation(batch) for batch in batches])
-
-        score = mean_util/len(batches)
-        for _ in range(10):
-            new_batches, new_batch_token_counts, new_batch_widths = self.merge_batches(batches, batch_token_counts, batch_widths)
-            mean_util = np.mean([self.batch_utilisation(batch) for batch in new_batches])
-            new_score = mean_util/len(new_batches)
-            # print(new_score, len(new_batches))
-            if new_score > score:
-                batches = new_batches
-                batch_token_counts = new_batch_token_counts
-                batch_widths = new_batch_widths
-                score = new_score
-            else:
-                break
-        return new_batches
 
     def create_batches(self):
+        # Get the sorting keys using the custom function
+        dataset_keys = [self.example_len(ex) for ex in self.dataset]
         indices = list(range(len(self.dataset)))
-        random.shuffle(indices)
-        sorted_indices = sorted(indices, key=lambda i: self._dataset_lens[i], reverse=False)
-
-        batches = [] 
-        batch = []
-        avg_batch_inp_len = 0
-        avg_batch_out_len = 0
-        max_batch_inp_len = 0
-        max_batch_out_len = 0
-        batch_token_counts = []
-        batch_token_count = 0
-        batch_widths = []
-
-
-        for idx in sorted_indices:
-            x, y = self.dataset[idx]
-            input_len = len(x.grid)
-            output_len = len(y.grid)
-
-            # Incremental stats
-            inc_max_inp_len = max(input_len, max_batch_inp_len)
-            inc_max_out_len = max(output_len, max_batch_out_len)
-
-            inc_avg_inp_len = (avg_batch_inp_len * len(batch) + input_len) / (len(batch) + 1)
-            inc_avg_out_len = (avg_batch_out_len * len(batch) + output_len) / (len(batch) + 1)
-
-            inc_inp_excess = (inc_max_inp_len - inc_avg_inp_len)
-            inc_out_excess = (inc_max_out_len - inc_avg_out_len)
-
-            inc_batch_util = 1.0 - ((inc_inp_excess + inc_out_excess) / (inc_max_inp_len + inc_max_out_len))
-            inc_batch_token_count = (inc_max_inp_len + inc_max_out_len + 3) * (len(batch) + 1)
-
-            if inc_batch_token_count >= self.approx_token_count or inc_batch_util < self.min_util:
-                batches.append(batch)
-                batch_token_counts.append(batch_token_count)
-                batch_widths.append(max_batch_inp_len + max_batch_out_len + 3)
-                batch = [idx]
-                avg_batch_inp_len = input_len
-                avg_batch_out_len = output_len
-                max_batch_inp_len = input_len
-                max_batch_out_len = output_len
-                batch_token_count = input_len + output_len + 3
-            else:
-                batch.append(idx)
-                avg_batch_inp_len = inc_avg_inp_len
-                avg_batch_out_len = inc_avg_out_len
-                max_batch_inp_len = inc_max_inp_len
-                max_batch_out_len = inc_max_out_len
-                batch_token_count = inc_batch_token_count 
-    
-        if len(batch) > 0:
-            batches.append(batch)
-            batch_token_counts.append(batch_token_count)
-            batch_widths.append(max_batch_inp_len + max_batch_out_len + 3)
-
-        batches = self.reduce_num_batches(batches, batch_token_counts, batch_widths)
-
-        return batches
-
-
-    def batch_utilisation(self, batch):
-        inp_lens = []
-        out_lens = []
+        # Sort indices based on the custom keys
+        sorted_indices = sorted(indices, key=lambda i: dataset_keys[i])
         
-        if len(batch) == 0:
-            return 0        
+        batches = []
+        current_batch = []
+        max_inp_len = 0
+        max_out_len = 0
+        
+        for idx in sorted_indices:
+            example = self.dataset[idx]
+            input_len = example.input.size
+            output_len = example.output.size
 
-        for i in batch:
-            x, y = self.dataset[i]
-            inp_lens.append(len(x.grid))
-            out_lens.append(len(y.grid))
+            potential_max_inp_len = max(max_inp_len, input_len)
+            potential_max_out_len = max(max_out_len, output_len)
+            potential_batch_size = len(current_batch) + 1
+            potential_batch_token_count = (potential_max_inp_len + potential_max_out_len + 3) * potential_batch_size
 
-        avg_inp_len = np.mean(inp_lens)
-        max_inp_len = np.max(inp_lens)
-        avg_out_len = np.mean(out_lens)
-        max_out_len = np.max(out_lens)
+            if potential_batch_token_count <= self.approx_token_count:
+                current_batch.append(idx)
+                max_inp_len = potential_max_inp_len
+                max_out_len = potential_max_out_len
+            else:
+                batches.append(current_batch)
+                current_batch = [idx]
+                max_inp_len = input_len
+                max_out_len = output_len
 
-        inp_excess = (max_inp_len - avg_inp_len) * len(batch)
-        out_excess = (max_out_len - avg_out_len) * len(batch)
+        if current_batch:
+            batches.append(current_batch)
 
-        inp_max = max_inp_len * len(batch)
-        out_max = max_out_len * len(batch)
-        utilisation = 1.0 - (inp_excess + out_excess) / (inp_max + out_max)  
-        return utilisation
+        self.batches = batches
+        return self.batches
 
     
     def __iter__(self):
@@ -212,7 +94,46 @@ class TargetTokenCountBatchSampler(BatchSampler):
     def __len__(self):
         # Experimentally this is only called when len(dataloader) is called
         return len(self.batches)
+    
+        
+    def __iter__(self):
+        # Shuffle the buckets and data within buckets if necessary
+        if self.shuffle:
+            self.shuffle_batches()
 
+        for batch in self.batches:
+            yield batch
+
+
+    def __len__(self):
+        # Experimentally this is only called when len(dataloader) is called
+        return len(self.batches)
+
+
+    def batch_utilisation(self, batch):
+        inp_lens = []
+        out_lens = []
+        
+        if len(batch) == 0:
+            return 0        
+        
+        for i in batch:
+            example: Example = self.dataset[i]
+            inp_lens.append(example.input.size)
+            out_lens.append(example.output.size)
+
+        avg_inp_len = np.mean(inp_lens)
+        max_inp_len = np.max(inp_lens)
+        avg_out_len = np.mean(out_lens)
+        max_out_len = np.max(out_lens)
+
+        inp_excess = (max_inp_len - avg_inp_len) * len(batch)
+        out_excess = (max_out_len - avg_out_len) * len(batch)
+
+        inp_max = max_inp_len * len(batch)
+        out_max = max_out_len * len(batch)
+        utilisation = 1.0 - (inp_excess + out_excess) / (inp_max + out_max)  
+        return utilisation
 
     def stats(self):
         utils = []
@@ -221,11 +142,13 @@ class TargetTokenCountBatchSampler(BatchSampler):
         for batch in self.batches:
             utils.append(self.batch_utilisation(batch))
             batch_lens.append(len(batch))
-            seq_len = 0
+            max_inp_len = 0
+            max_out_len = 0
             for i in batch:
-                x, y = self.dataset[i]
-                seq_len = max(len(x.grid) + len(y.grid) + 3, seq_len)
-            token_counts.append(seq_len * len(batch))
+                example: Example = self.dataset[i]
+                max_inp_len = max(max_inp_len, example.input.size)
+                max_out_len = max(max_out_len, example.output.size)
+            token_counts.append((max_inp_len + max_out_len + 3) * len(batch))
             
 
         logger.info(f"Number of Batches: {len(self.batches)}")
@@ -244,6 +167,7 @@ class TargetTokenCountBatchSampler(BatchSampler):
         print_stats("Token Count", token_counts)
 
 
+
 class ArcExamplesDataset(Dataset):
     def __init__(self, examples: List[Example], tokenizer: ArcTokenizer):
 
@@ -251,13 +175,12 @@ class ArcExamplesDataset(Dataset):
         # Or the sampler will break
         self.examples = examples
         self.tokenizer = tokenizer
-        self.x, self.y = zip(*[self.tokenizer.encode(e) for e in examples])
 
     def __len__(self):
-        return len(self.x)
+        return len(self.examples)
 
     def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
+        return self.examples[idx]
     
     def subset(self, num_examples: int):
         """
@@ -270,9 +193,8 @@ class ArcExamplesDataset(Dataset):
         indices = list(range(num_examples))
         return ArcExamplesDataset([self.examples[i] for i in indices], self.tokenizer)
     
-    @staticmethod
-    def collate_fn(batch: List[Tuple[MODEL_INPUT, MODEL_OUTPUT]], pad_idx: int, device=torch.device('cpu'))-> Tuple[MODEL_INPUT, MODEL_OUTPUT]:
-        x, y  = zip(*batch)
+    def collate_fn(self, batch: List[Example], pad_idx: int, device=torch.device('cpu'))-> Tuple[MODEL_INPUT, MODEL_OUTPUT]:
+        x, y = zip(*[self.tokenizer.encode(ex) for ex in batch])
 
         cps = [xi.color_permutation for xi in x]
         ats = [xi.array_transform for xi in x]
