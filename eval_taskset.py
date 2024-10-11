445837b876870133b21e01282438d5c0d01da2c4
Abhi Agg <abhiggarwal@gmail.com>
2024-10-11 15:49:58 +0000

    updated eval_taskset to use cuda device

--- eval_taskset.py
+++ eval_taskset.py
@@ -7,8 +7,11 @@ from src.repl import REPLConfig, REPL
 from src.task import ARC_TRAIN, ARC_EVAL
 from src.tokenizer import ArcTokenizer
 from src.dataset import ArcExamplesDataset
+from src.utils import map_to_tensors
 from tqdm import tqdm
-ckt_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/models/v9/D512E128H16B5I3.v1/ckt_162000_39.205.pth'
+
+ckt_path = '/teamspace/studios/work-horse/ArcSolver/runs/v9/D512E128H16B5I3.v1/ckt_281000_52.168.pth'
+# ckt_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/models/v9/D512E128H16B5I3.v1/ckt_162000_39.205.pth'
 #%%
 ## Load model
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -18,18 +21,20 @@ model = REPL(model_config)
 model.load_state_dict(data['model_state_dict'], strict=True)
 tokenizer = ArcTokenizer.from_dict(data['tokenizer'])
 model = torch.jit.script(model)
+model.to(device)
 model.eval()
 #%%
-def get_predicted_tokens(model, tokenizer, example):
+def get_predicted_tokens(model, tokenizer, example, device):
     pad_idx = tokenizer.grid_tokenizer.PAD_IDX
     start_idx = tokenizer.grid_tokenizer.BOS_IDX
-    x, y =  ArcExamplesDataset.collate_fn([example], 
+    batch =  ArcExamplesDataset.collate_fn([example], 
                                 pad_idx=pad_idx, 
                                 tokenizer=tokenizer,
                                 permute=False, 
                                 keep_meta=False,
                                 device=torch.device('cpu'))
 
+    x, y = map_to_tensors(batch, lambda x: x.to(device, non_blocking=True))
     iter_logits, _ = model(x, y)
     logits = iter_logits
     # logits = iter_logits[-1]
@@ -62,12 +67,12 @@ for task in tqdm(tasks):
     }
 
     for example in task.train:
-        predicted_tokens, target_tokens = get_predicted_tokens(model, tokenizer, example)
+        predicted_tokens, target_tokens = get_predicted_tokens(model, tokenizer, example, device)
         is_match = predicted_tokens == target_tokens
         result['train'].append(is_match)
 
     for example in task.test:
-        predicted_tokens, target_tokens = get_predicted_tokens(model, tokenizer, example)
+        predicted_tokens, target_tokens = get_predicted_tokens(model, tokenizer, example, device)
         is_match = predicted_tokens == target_tokens
         result['test'].append(is_match)
 
