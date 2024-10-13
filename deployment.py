9177239c21bd86a8b7fa263b522aadd23fa5434e
Abhi Agg <abhiggarwal@gmail.com>
2024-10-13 00:45:27 +0000

    update

--- deployment.py
+++ deployment.py
@@ -4,14 +4,18 @@ from src.solver import create_solver
 import torch
 torch.manual_seed(42)
 #%%
-tasks_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/data/arc_kaggle_data/arc-agi_evaluation_challenges.json'
-solution_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/data/arc_kaggle_data/arc-agi_evaluation_solutions.json'
+# base_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/'
+base_path = '/teamspace/studios/this_studio/ArcSolveR/'
+tasks_path = base_path + 'data/arc_kaggle_data/arc-agi_evaluation_challenges.json'
+solution_path = base_path + 'data/arc_kaggle_data/arc-agi_evaluation_solutions.json'
 tasks = load_tasks(tasks_path, solution_path)
 #%%
 # ckt_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/models/v9/D512E128H16B5I3.v1/D512E128H16B5I3.v1_ckt_281000_52.168.pt'
-ckt_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/models/v9/D512E128H16B5I3.v1/ckt_281000_52.168.pth'
+# ckt_path = '/Users/abhishekaggarwal/synced_repos/ArcSolver/models/v9/D512E128H16B5I3.v1/ckt_281000_52.168.pth'
+ckt_path = '/teamspace/studios/work-horse/ArcSolver/runs/v9/D512E128H16B5I3.v1/ckt_281000_52.168.pth'
 solver = create_solver(ckt_path)
 # solver = Solver(ckt_path=ckt_path, bs=5, patience=10)
 # solver = torch.jit.script(solver)
 #%%
 solver(tasks[0], seed=1, thinking_duration=300, min_confidence=0.001)
+# %%
